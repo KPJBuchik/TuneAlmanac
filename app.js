3fa86b8d8558b6a423a2600e52f65625045b5c1a
Kevin Buchik <kbuchik@gmail.com>
2021-01-14 13:13:43 -0700

    playlisttrack function

--- app.js
+++ app.js
@@ -569,7 +569,7 @@ for (var i = 0; i < allPlaylistTracks.length; i++) {
 function getPlaylistTracks() {
 
         
-        // var playlistId = $(this).attr("value")
+         var playlistId = document.getElementsByClassName("playlist").setAttribute("value")
       
 
 
